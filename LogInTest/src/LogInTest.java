62d5476df4c9be9dda676df02188ea2a24e20fae
Mp3103 <57645693+Mp3103@users.noreply.github.com>
2020-08-26 19:50:52 -0500

    Update LogInTest.java

--- LogInTest/src/LogInTest.java
+++ LogInTest/src/LogInTest.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class LogInTest {
 	public static void main(String[] args) {
 		
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\15145\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","YOUR WEB DRIVER PATH");
 		
 		WebDriver webDriver = new ChromeDriver();
 		
